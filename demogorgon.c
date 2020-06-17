cd9189ab99e50d1cde3baafd9c851bec2527a411
GabrielaLescano <65716218+GabrielaLescano@users.noreply.github.com>
2020-06-17 17:52:25 -0300

    Update demogorgon.c

--- demogorgon.c
+++ demogorgon.c
@@ -1,10 +1,10 @@
 #include "demogorgon.h"
 
 
-#define MSJ_ENFRENTAMIENTO "\n\nEl enfrentamiento est%c por comenzar\n"
+#define MSJ_ENFRENTAMIENTO "\n\nEl enfrentamiento est%c por comenzar\n",160
 
-#define MSJ_MAL_INGRESO "Ingreso no v%clido\n"
-#define MSJ_RECIBIR_ANIMO "\nIngrese el %cnimo de El(Eleven) en un rango del 1 al 100 inclusive\n"
+#define MSJ_MAL_INGRESO "Ingreso no v%clido\n",160
+#define MSJ_RECIBIR_ANIMO "\nIngrese el %cnimo de El(Eleven) en un rango del 1 al 100 inclusive\n",160
 
 const int DEF_ANIMO = 0;
 const int MIN_ANIMO = 1;
@@ -13,7 +13,7 @@ const int MAX_ANIMO = 100;
 const bool ANIMO_VERIFICADO = true;
 const bool ANIMO_NO_VERIFICADO = false;
 
-#define MSJ_RECIB_CONFIANZA "Ingrese el nivel de confianza que tiene El(Eleven) con sus amigos\nBajo [b] - Medio [m] - Alto [a]\n"
+#define MSJ_RECIBIR_CONFIANZA "\nIngrese el nivel de confianza que tiene El(Eleven) con sus amigos\nBajo [b] - Medio [m] - Alto [a]\n"
 
 const char DEF_NIVEL_CONFIANZA = 'z';
 #define CONFIANZA_BAJA 'b'
@@ -36,32 +36,32 @@ const float MIN_GANO = 67.0F;
 const float MAX_GANO = 370.0F;
 const float VALOR_GANO_JUSTO = 65.0F;
 
-#define MSJ_ERROR_CALCULAR_FUERZA "Algo malio sal"
+#define MSJ_ERROR_CALCULAR_FUERZA "Algo mali%c sal",162
 
 
-#define MSJ_GANO "\n\n\n\n\tEleven conto con su mayor animo y con la confianza de sus amigos\n\tpor lo que logro vencer al Demogorgon con su fuerza psquica!!!\n\n\n\t \n"
+#define MSJ_GANO "\n\n\n\n\tEleven cont%c con su mayor %cnimo y con la confianza de sus amigos\n\tpor lo que logr%c vencer al Demogorgon con su fuerza ps%cquica!!!\n\n\n\t \n"162,160,162,161
 
-#define MSJ_GANO_JUSTO "\n\n\n\n\tEleven conto con la fuerza psiquica justa para poder vencer al Demogorgon\n\tpor lo que quedo realmente debil luego del enfrentamiento!\n\n\n"
+#define MSJ_GANO_JUSTO "\n\n\n\n\tEleven cont%c con la fuerza ps%cquica justa para poder vencer al Demogorgon\n\tpor lo que qued%c realmente d%cbil luego del enfrentamiento!\n\n\n"162,161,162,130
 
-#define MSJ_RESULT_DESC "\n\n\n\n\tEleven enfrento al Demogorgon pero no parecia que lo pudiera vencer\n\thasta que una luz cubrio todo el lugar desapareciendo al Demogorgon junto a Eleven\n\n\tFin.. ?\n\n"
+#define MSJ_RESULT_DESC "\n\n\n\n\tEleven enfrent%c al Demogorgon pero no parec%ca que lo pudiera vencer\n\thasta que una luz cubri%c todo el lugar desapareciendo al Demogorgon junto a Eleven\n\n\tFin.. ?\n\n"102,161,162
 
 
 
 void comenzar_enfrentamiento(){
 
-	printf(MSJ_ENFRENTAMIENTO,160);
-	//160 = á
+	printf(MSJ_ENFRENTAMIENTO);
+	
 }
 
 int recibir_animo(){
 	int animo_recibido = DEF_ANIMO;
 	do{
 
-		printf(MSJ_RECIBIR_ANIMO,160);
+		printf(MSJ_RECIBIR_ANIMO);
 		scanf("%i", &animo_recibido);
 
 		if( (animo_recibido < MIN_ANIMO ) || (animo_recibido > MAX_ANIMO) ){
-		printf (MSJ_MAL_INGRESO,160);
+		printf (MSJ_MAL_INGRESO);
 		}else{
 			printf("Ingreso aceptado\n");
 		}
@@ -70,7 +70,6 @@ int recibir_animo(){
 	return animo_recibido;
 }
 
-
 char recibir_confianza(){
 	char confianza_recibida = DEF_NIVEL_CONFIANZA;
 
@@ -78,9 +77,9 @@ char recibir_confianza(){
 		printf(MSJ_RECIBIR_CONFIANZA);
 		scanf("%c", &confianza_recibida);
 		if(confianza_recibida == CONFIANZA_BAJA || confianza_recibida == CONFIANZA_MEDIA || confianza_recibida == CONFIANZA_ALTA){
-			printf("Ingreso de confianza aceptado\n");
+			printf("Ingreso de confianza aceptado");
 		}else{
-			printf(MSJ_MAL_INGRESO,160);
+			printf(MSJ_MAL_INGRESO);
 		}
 	}while ( ((confianza_recibida!=CONFIANZA_BAJA ) && (confianza_recibida!=CONFIANZA_MEDIA)) && (confianza_recibida!=CONFIANZA_ALTA));
 	
@@ -90,9 +89,29 @@ char recibir_confianza(){
 
 
 float recibir_fuerza_psiquica(int nivel_animo, char nivel_confianza){
+	float multiplicador = 0.0F;
+	
+	switch(nivel_confianza){
+		case CONFIANZA_BAJA:
+			multiplicador = MULTIP_CONF_BAJO;
+			break;
+		case CONFIANZA_MEDIA:
+			multiplicador = MULTIP_CONF_MEDIO;
+			break;
+		default:
+			multiplicador = MULTIP_CONF_ALTO;
+	}
+
+	return calcular_fuerza(nivel_animo, multiplicador);
+
 }
+
+
 float calcular_fuerza(int nivel_animo, float multiplicador_confianza){
+	return (nivel_animo * multiplicador_confianza);
 }
+
+/*
 void resultado_enfrentamiento(float fuerza_psiquica){
 }
 char enfrentamiento(float fuerza_psiquica){
